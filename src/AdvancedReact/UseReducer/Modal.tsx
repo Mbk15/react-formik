513e2342993764f51313490489ac5737d458be05
Mbk15 <mubarakmuhammadjamiu@gmail.com>
2022-06-16 17:54:06 +0100

    Use Reducer For Modal close and reducer action separation of concern

--- src/AdvancedReact/UseReducer/Modal.tsx
+++ src/AdvancedReact/UseReducer/Modal.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Modal = ({ modalContent }) => {
+const Modal = ({ modalContent, closeModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      closeModal();
+    }, 3000);
+  });
+
   return (
     <div className="bg-gray-200 text-red-500 font-bold text-center w-1/2 mx-auto my-4 ">
       {modalContent}
